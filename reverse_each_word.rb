c3e1969be791a62013a61befcb889737871a3680
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 17:51:58 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
-def reverse_each_word(sentence) 
-  reversed_words_in_sentence = sentence.split(" ").collect do |word| 
-    word.reverse
-  end 
-  reversed_words_in_sentence.join(" ")
+def reverse_each_word(string)
+  string_into_array = string.split(" ")
+  reverse_str = []
+  string_into_array.each do |word|
+    reverse_str.push(word.reverse)
+  end
+  return reverse_str.join(" ")
 end
